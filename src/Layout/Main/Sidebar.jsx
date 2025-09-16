2a167375947a672eb3249b6a731db682e8ba84da
Md Jowel Ahmed <155435567+MdJowelAhmed@users.noreply.github.com>
2025-09-16 20:25:33 +0600

    update header

--- src/Layout/Main/Sidebar.jsx
+++ src/Layout/Main/Sidebar.jsx
@@ -230,7 +230,7 @@ const Sidebar = ({ collapsed, setCollapsed }) => {
     style={{ width: collapsed ? 80 : 320 }}
     >
       {/* Toggle Button */}
-      <div
+      {/* <div
         className="flex justify-end items-center p-2 cursor-pointer"
         onClick={() => setCollapsed(!collapsed)}
       >
@@ -239,7 +239,7 @@ const Sidebar = ({ collapsed, setCollapsed }) => {
         ) : (
           <MenuFoldOutlined style={{ fontSize: 20 }} />
         )}
-      </div>
+      </div> */}
 
       {/* Logo + Upload */}
       {!collapsed && (
